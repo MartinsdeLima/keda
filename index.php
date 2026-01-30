23430960a694f261739eab2f9f50d32605dc7858
Martins de Lima <martins.lima@sulamerica.com.br>
2026-01-30 14:06:07 -0300

    Nova versão ARM-2

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
 
-header('Content-Type: text/plain; charset=utf-8');
-
-$uri = $_SERVER['REQUEST_URI'] ?? '/';
-$uri = parse_url($uri, PHP_URL_PATH);
-
-if ($uri === '/ping') {
+if ($_SERVER['REQUEST_URI'] === '/ping') {
     echo "pong";
-    http_response_code(200);
     exit;
 }
 
-if ($uri === '/') {
-    echo "Hello from PHP!";
-    http_response_code(200);
-    exit;
-}
-
-echo "Not Found";
-http_response_code(404);
+echo "Hello from PHP!";
